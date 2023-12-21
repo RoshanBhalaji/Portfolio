92775b354ee89dca19632332b577da7cd568e2a7
RoshanBhalaji <roshanbhalaji5@gmail.com>
2023-12-21 22:32:04 +0530

    IntialCommit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed(".auto-type", {
+        strings: ["Roshan ", "Coding &#128187;", "Sleeping &#128564;", "Eating &#127869;"],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true
+    });
+});
+
